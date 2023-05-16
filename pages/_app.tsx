94a26533adbacd4b751a0ba3ddb3420eb63b97c8
Alif Siregar <alifsiregar@Alifs-MacBook-Pro.local>
2023-05-16 10:42:38 +0700

    feat: adjust _app.tsx to include layout and montserrat font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// google font
+import { Montserrat } from 'next/font/google';
+
+// component
+import Layout from '@/components/Layout';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={montserrat.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>  
+  )
 }
