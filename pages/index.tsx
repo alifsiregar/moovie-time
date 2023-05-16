89d900d4202afcb119eed88c1fdfe874c4437c38
Alif Siregar <alifsiregar@Alifs-MacBook-Pro.local>
2023-05-16 12:31:16 +0700

    feat: add buttons to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,43 @@
 // components
 import MovieCard from "@/components/MovieCard";
+import Title from "@/components/Title";
+import Button from "@/components/Button";
 
 export default function Home() {
   return (
     <>
     <div className="px-[120px] pb-[120px] flex flex-col relative before:content-[''] before:h-[333px] before:bg-bgGray-100 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-[15px]">
       <div className="relative mt-[120px]">
-        <span className=" relative text-textWhite font-[500] text-[24px] before:content-[''] before:w-[112px] before:h-[6px] before:bg-[#E74C3C] before:absolute before:bottom-[45px]">
-          Discover Movies
-        </span>
+        <div className=" flex justify-between">
+          <Title
+            text="Discover Movies"
+          />
+          <div className=" flex gap-[20px]">
+            <Button
+              option={1}
+              customStyle=" px-[16px]"
+            >
+              <span className=" text-[14px] font-[500] text-textWhite">
+                Popularity
+              </span>
+            </Button>
+            <Button
+              option={2}
+              customStyle=" pl-[18px] pr-[15px]"
+            >
+              <span className=" text-[14px] font-[500] text-textWhite">
+                Release Date
+              </span>
+            </Button>
+          </div>
+        </div>
         <div className="mt-[43px] relative min-h-[120px] text-textWhite grid grid-cols-5 gap-y-[37px] gap-x-[25px]">
           {new Array(10).fill(0).map((_, idx) => {
             return(
               <MovieCard key={String(idx)} />
             )
           })}
-      </div>
+        </div>
       </div>
     </div>
     </>
